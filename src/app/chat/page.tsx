0c22e734fde87986da8c618f0b36f05136269f95
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-09 22:35:17 -0300

    create form

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
 import { redirect } from "next/navigation";
+import Form from "../components/Form";
 
 export default async function ChatHomePage() {
   const session = await getServerSession(authOptions);
@@ -9,5 +10,9 @@ export default async function ChatHomePage() {
     redirect("/");
   }
 
-  return <div className="h-screen bg-gray-200 flex flex-col"></div>;
+  return (
+    <div className="h-screen bg-gray-200 flex flex-col">
+      <Form />
+    </div>
+  );
 }
---
+++ src/app/components/Form.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function Form() {
+  return (
+    <form className="p-6 fixed bottom-0 left-0 w-full bg-white">
+      <div className="flex">
+        <input
+          type="text"
+          name="message"
+          placeholder="Type your message..."
+          className="flex-grow py-2 px-4 outline-none"
+        />
+        <button
+          type="submit"
+          className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-lg"
+        >
+          Send
+        </button>
+      </div>
+    </form>
+  );
+}
