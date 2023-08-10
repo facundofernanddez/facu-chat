b884e65e165e7b63ed82f5eee316561b5a37d517
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-10 08:05:51 -0300

    add form action

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import postData from "../action";
+
 export default function Form() {
   return (
-    <form className="p-6 fixed bottom-0 left-0 w-full bg-white">
+    <form
+      action={async (formData) => {
+        await postData(formData);
+      }}
+      className="p-6 fixed bottom-0 left-0 w-full bg-white"
+    >
       <div className="flex">
         <input
           type="text"
