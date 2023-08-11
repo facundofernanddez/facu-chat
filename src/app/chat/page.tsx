0b7e6e74fcc83f797b6cecee9b1d0a727f531200
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-11 09:49:45 -0300

    rendering messages

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,9 +2,33 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
 import { redirect } from "next/navigation";
 import Form from "../components/Form";
+import { prisma } from "../lib/db";
+import ChatComponent from "../components/Chat";
+
+const getData = async () => {
+  const data = await prisma.message.findMany({
+    select: {
+      message: true,
+      id: true,
+      User: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 50,
+  });
+
+  return data;
+};
 
 export default async function ChatHomePage() {
   const session = await getServerSession(authOptions);
+  const data = await getData();
 
   if (!session) {
     redirect("/");
@@ -12,6 +36,7 @@ export default async function ChatHomePage() {
 
   return (
     <div className="h-screen bg-gray-200 flex flex-col">
+      <ChatComponent data={data as any} />
       <Form />
     </div>
   );
