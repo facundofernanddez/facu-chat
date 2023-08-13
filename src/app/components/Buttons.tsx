569aa6ff39a783328620c66412f076d92691e973
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-13 12:29:00 -0300

    revert changes

--- src/app/components/Buttons.tsx
+++ src/app/components/Buttons.tsx
@@ -16,13 +16,7 @@ export function Logout() {
 export function NavLogin() {
   return (
     <button
-      onClick={() =>
-        signIn("github", {
-          redirect: true,
-          callbackUrl: "/",
-          redirectUrl: "https://facu-chat.vercel.app/",
-        })
-      }
+      onClick={() => signIn("github")}
       className="flex items-center justify-center gap-2 rounded-lg bg-indigo-600 px-8 py-3 texte-center text-sm font-semibold text-white ring-indigo-300 transition duration-100 hover:bg-indigo-700 md:text-base"
     >
       Login
