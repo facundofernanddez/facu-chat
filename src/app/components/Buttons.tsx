077e25ac5af576f520fe05dd0048235cfb113ba7
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-13 11:37:26 -0300

    change callback url

--- src/app/components/Buttons.tsx
+++ src/app/components/Buttons.tsx
@@ -19,7 +19,7 @@ export function NavLogin() {
       onClick={() =>
         signIn("github", {
           redirect: true,
-          callbackUrl: "/chat",
+          callbackUrl: "/",
         })
       }
       className="flex items-center justify-center gap-2 rounded-lg bg-indigo-600 px-8 py-3 texte-center text-sm font-semibold text-white ring-indigo-300 transition duration-100 hover:bg-indigo-700 md:text-base"
