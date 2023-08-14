c1b3fad618b0517fd0ef5256f12c37bba3310b89
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-14 07:36:00 -0300

    revert changes

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -2,11 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 import type { ReactNode } from "react";
-import { Session } from "next-auth";
 
-export const NextAuthProvider = (
-  { children }: { children: ReactNode },
-  session: Session
-) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+export const NextAuthProvider = ({ children }: { children: ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>;
 };
--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,7 +1,6 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "./db";
 
 export const authOptions: NextAuthOptions = {
@@ -11,10 +10,6 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_SECRET_ID as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
 };
